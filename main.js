1904c315c8cbea768c0b25da8d366588f1b82898
erindhoxha <hello@erindhoxha.dev>
2024-04-25 11:36:33 +0200

    Added line count

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import "./assets/css/tailwind.css";
+
+let textbox = document.getElementById('followers-textbox');
+textbox.addEventListener('input', function() {
+    let lines = textbox.value.split('\n').length;
+    if (textbox.value.length === 0) {
+        lines = 0;
+    }
+    let title = document.getElementById("followers-count")
+    title.textContent = `${lines}`;
+});
+
+let followingsTextbox = document.getElementById('followings-textbox');
+followingsTextbox.addEventListener('input', function() {
+    let lines = followingsTextbox.value.split('\n').length;
+    if (followingsTextbox.value.length === 0) {
+        lines = 0;
+    }
+    let title = document.getElementById("followings-count")
+    title.textContent = `${lines}`;
+});
+
